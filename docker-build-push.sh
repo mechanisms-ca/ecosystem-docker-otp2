0c15ac614439c10de433f28f550f55788ffbb739
Nathan <nathan.schepers@gmail.com>
2021-12-24 13:11:25 +0000

    rename and add registry option to docker build script

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+REGISTRY='docker.io'
 IMAGE_NAME='mechanisms/otp2'
 SHORT_HASH=$(git rev-parse --short HEAD)
 VERSION=${1}
@@ -8,13 +9,13 @@ VERSION=${1}
 if [ -z "$VERSION" ]; then
   echo "building $IMAGE_NAME:$SHORT_HASH"
   docker build -t $IMAGE_NAME:$SHORT_HASH .
-  echo "pushing $IMAGE_NAME:$SHORT_HASH"
-  docker push $IMAGE_NAME:$SHORT_HASH
+  echo "pushing ${REGISTRY}/$IMAGE_NAME:$SHORT_HASH"
+  docker push ${REGISTRY}/$IMAGE_NAME:$SHORT_HASH
 else
   echo "version is '$VERSION'";
   docker build -t $IMAGE_NAME:$SHORT_HASH -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest .
-  echo "pushing $IMAGE_NAME:$SHORT_HASH, $IMAGE_NAME:$VERSION, $IMAGE_NAME:latest"
-  docker push --all-tags $IMAGE_NAME
+  echo "pushing ${REGISTRY}/$IMAGE_NAME:$SHORT_HASH, ${REGISTRY}/$IMAGE_NAME:$VERSION, ${REGISTRY}/$IMAGE_NAME:latest"
+  docker push --all-tags ${REGISTRY}/$IMAGE_NAME
 fi
 
 
