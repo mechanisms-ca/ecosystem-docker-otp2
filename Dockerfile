8de79bcbe518d88e4fe262ea07817210402d02dd
Nathan <nathan.schepers@gmail.com>
2022-08-20 13:41:50 +0100

    cleanup and limit scope. fix tags and versions

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openjdk:11-jre-slim
 LABEL maintainer="Nathan Schepers <nathan.schepers@protonmail.com>"
 
-ENV OTP_VERSION=2.0.0
+ENV OTP_VERSION=2.1.0
 ENV JAVA_OPTIONS=-Xmx1G
 
 ADD https://repo1.maven.org/maven2/org/opentripplanner/otp/$OTP_VERSION/otp-$OTP_VERSION-shaded.jar /usr/local/share/java/
--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REGISTRY='docker.io'
-IMAGE_NAME='mechanisms/otp2'
+IMAGE_NAME='mechanisms/otp'
 SHORT_HASH=$(git rev-parse --short HEAD)
 VERSION=${1}
 
@@ -13,8 +13,8 @@ if [ -z "$VERSION" ]; then
   docker push ${REGISTRY}/$IMAGE_NAME:$SHORT_HASH
 else
   echo "version is '$VERSION'";
-  docker build -t $IMAGE_NAME:$SHORT_HASH -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest .
-  echo "pushing ${REGISTRY}/$IMAGE_NAME:$SHORT_HASH, ${REGISTRY}/$IMAGE_NAME:$VERSION, ${REGISTRY}/$IMAGE_NAME:latest"
+  docker build -t $IMAGE_NAME:$VERSION .
+  echo "pushing ${REGISTRY}/$IMAGE_NAME:$VERSION"
   docker push --all-tags ${REGISTRY}/$IMAGE_NAME
 fi
 
